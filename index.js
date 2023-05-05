d676e98310651e1f1183dec6c2347666146ab0ac
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-05-05 14:33:54 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === attribute.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()
+    })
+}
+
+function matchName(genericDrivers, driverNames) {
+    return genericDrivers.filter(function(driver) {
+        return driver.name === driverNames
+    })
+}
